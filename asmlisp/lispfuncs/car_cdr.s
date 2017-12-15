69a2d0474f733b3f284962807ffd5a25f8ee8377
Mark Johnson <Mark.Johnson.62899@gmail.com>
2017-12-15 01:55:00 -0600

    car now returns nil for atoms

--- asmlisp/lispfuncs/car_cdr.s
+++ asmlisp/lispfuncs/car_cdr.s
@@ -1,9 +1,17 @@
 .type	car, @function
 car:
+	cmpq	$0, (%rdi)
+	jne	.nil_ret
 	movq	8(%rdi), %rax
 	ret
 
 .type	cdr, @function
 cdr:
+	cmpq	$0, (%rdi)
+	jne	.nil_ret
 	movq	16(%rdi), %rax
+	ret
+
+.nil_ret:
+	leaq	NIL(%rip), %rax
 	ret
